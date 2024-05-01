ce210b925c28d7f44ded511b7c18b1c1a2258124
edsonurano <edsonurano@unifor.br>
2024-05-01 09:43:36 -0300

    kruskal implementation

--- Kruskal/Kruskal.py
+++ Kruskal/Kruskal.py
@@ -1,20 +1,56 @@
+import heapq
+
 kruskalFile = open('kruskal.txt', 'r')
 kruskalNumEdges = int(kruskalFile.readline())
 kruskaEdgesWeight = [float(x) for x in kruskalFile.read().split()]
+kruskalFontEdge = int(kruskaEdgesWeight.pop(-1))
 
 def adjustMatrix(vectorEdgesWeight):
     matrix = []
     for i in range(kruskalNumEdges):
         row = []
         for j in range(kruskalNumEdges):
-            row.append([float(vectorEdgesWeight.pop(0)), i+1, j+1])
-        matrix.append(row)
+            edgeWeigth = vectorEdgesWeight.pop(0)
+            if(edgeWeigth != 0):
+                row.append([edgeWeigth, i, j])
+        if(len(row) != 0):
+            matrix.append(row)
     return matrix
 
+def sortEdges(matrix):
+    sortedEdges = []
+    for row in matrix:
+        for edge in row:
+            sortedEdges.append(edge)
+    sortedEdges.sort(key=lambda x: x[0])
+    return sortedEdges
+
+def makeSet(numEdges):
+    sets = [[] * numEdges for i in range(numEdges)]
+    belongsTo = []
+    for i in range(numEdges):
+        sets[i].append(i)
+        belongsTo.append(i)
+    return sets, belongsTo
+
+def findSet(belongsTo, edge):
+    return belongsTo[edge]
+
+def union(belongsTo, edge1, edge2):
+    belongsTo[edge1] = edge2
+
 def kruskal(numEdges, vectorEdgesWeight):
-    A = None
+    A = []
     adjustedMatrix = adjustMatrix(vectorEdgesWeight)
-    print(adjustedMatrix)
+    sortedEdges = sortEdges(adjustedMatrix)
+    sets, belongsTo = makeSet(numEdges)
+
+    for edge in sortedEdges:
+        if(findSet(belongsTo, edge[1]) != findSet(belongsTo, edge[2])):
+            A.append(edge)
+            union(belongsTo, findSet(belongsTo, edge[1]), findSet(belongsTo, edge[2]))
+
+    
     return A
     
 
