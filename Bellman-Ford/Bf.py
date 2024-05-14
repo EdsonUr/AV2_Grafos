4f66fceaae8422ce03e2b5c0dbe1c87129d79b46
edsonurano <edsonurano@unifor.br>
2024-05-13 21:40:11 -0300

    bf

--- Bellman-Ford/Bf.py
+++ Bellman-Ford/Bf.py
@@ -1,22 +1,22 @@
 bellFile = open('bf.txt', 'r')
 bellNumVertices = int(bellFile.readline())
 
-G = [] ##GRAPH
-V = [] ##VERTICES
-E = [] ##EDGES
-w = [] ##WEIGHTS
+G = []  # GRAPH
+V = []  # VERTICES
+E = []  # EDGES
+w = []  # WEIGHTS
 
 for vertice in range(bellNumVertices):
     V.append(vertice)
     w.append([0] * bellNumVertices)
     rowEdges = [float(x) for x in bellFile.readline().split()]
 
-    for edge in rowEdges:
+    for idx, edge in enumerate(rowEdges):
         if edge != 0:
-            E.append([vertice, rowEdges.index(edge)])
-            w[vertice][rowEdges.index(edge)] = edge
+            E.append([vertice, idx])
+            w[vertice][idx] = edge
 
-s = int(bellFile.readline())##ROOT
+s = int(bellFile.readline())  # ROOT
 G.append(V)
 G.append(E)
 
@@ -48,7 +48,5 @@ def bellmanFord(G, w, s):
         if d[edge[1]] > d[edge[0]] + w[edge[0]][edge[1]]:
             return {'success': False}  # Indica a presença de um ciclo negativo
     return {'success': True, 'distances': d, 'predecessors': pi}
-
-
 
 print(bellmanFord(G, w, s))
